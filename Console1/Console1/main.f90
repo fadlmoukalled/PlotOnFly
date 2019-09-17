a5ee4eab95721cd8302ba463476a2cd5c7598cf6
Fadl Moukalled <fmukalled@aub.edu.lb>
2019-09-17 23:45:16 +0300

    Plotting

--- Console1/Console1/main.f90
+++ Console1/Console1/main.f90
@@ -1,57 +1,59 @@
 program main
   implicit none
-  integer, parameter :: n = 2000
+  integer, parameter :: n = 10000
   integer ( kind = 4 ) i
-  real ( kind = 8 ) :: x(n), y(n)
+  real ( kind = 8 ) :: x(n)
+  character(len = 200) :: PlotLine
+  integer, parameter :: nVariables = 5 !assign the number of variables here
+  character (len = 30) :: nVariablesChar
   
     
+    write(nVariablesChar , *) nVariables
+    
+    PlotLine = 'plot for [i=1:'//trim(adjustl(nVariablesChar))//'] "data.txt" using 1:i with lines title columnheader(i)'
     !--------------------------------------------------------------------------!
+    1   format (6e20.12)
+    open ( unit = 4, file = "data.txt")
+    !write titles of the graphs here
+    write ( 4,*) "Title1 ", "Title2 ", "Title3 ", "Title4 ", "Title5 "
+  
+    !--------------------------------------------------------------------------!
+    open ( unit = 2, file = "commands2.txt")
+    write(2,*) 'a = 0'
+    write(2,*) 'trim(PlotLine)'
+    close(2) 
+    !--------------------------------------------------------------------------!
+    !Write the options here
     open ( unit = 1, file = "commands1.txt")
     write(1,*) 'set title "GNUFOR plot" '
     write(1,*) 'set xlabel "x" '
     write(1,*) 'set ylabel "y" '
     write(1,*) 'set grid'
-    write(1,*) 'set log y'
+!    write(1,*) 'set log y'
+    write(1,*) 'load "commands2.txt" ' 
+    write(1,*) 'if(a==1) '//trim(PlotLine)
+    write(1,*) 'reread'
     close(1)
-    !CALL system('gnuplot commands1.txt')
-    
     !--------------------------------------------------------------------------!
-    
-    open ( unit = 2, file = "commands2.txt")
-    write(2,*) 'a = 0'
-    write(2,*) 'plot "data.txt" using 1:2 with lines'
-    close(2) 
-    
+    call system('start "" gnuplot commands1.txt ')
     !--------------------------------------------------------------------------!
-    
-    open ( unit = 3, file = "commands3.txt")
-    write(3,*) 'load "commands2.txt"' 
-    write(3,*) 'load "commands1.txt"'
-    write(3,*) 'if(a==1) plot "data.txt" using 1:2 with lines'
-    write(3,*) 'reread'
-    close(3) 
-    !--------------------------------------------------------------------------!
-    call system('start "" gnuplot commands3.txt ')
-    !--------------------------------------------------------------------------!
-    
-    
-    open ( unit = 4, file = "data.txt")
+ 
     do i = 1, n
-        x(i) = i
-        y(i) = x(i)**2
+        x(i) = i 
         
-        write ( 4, * ) x(i), y(i)
+        write ( 4,1) x(i), 2*x(i), 3*x(i), 4*x(i), 5*x(i), 6*x(i)
         
-        
+        !--------------------------------------------------------------!
+        !Should be added directly after writing residuals to the files
         open ( unit = 2, file = "commands2.txt")
         write(2,*) 'a=1'
         close(2)
-        
         open ( unit = 2, file = "commands2.txt")
         write(2,*) 'a=0'
         close(2)
+        !--------------------------------------------------------------!
     
-!    call sleep(1)
+!    call sleep(1/100)
     end do
   stop
 end
